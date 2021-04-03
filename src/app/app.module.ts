fbcdcfa3ef7a6abc9dc57c30df7f303b4f099f1c
unknown <rawyachakroun1@gmail.com>
2021-04-03 20:52:56 +0100

    try to commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { FollowedComponent } from './components/followed/followed.component';
 // Services
 import {UserService} from './services/user.service';
 import {UserGuard} from './services/user.guard';
+import { ThematiquesComponent } from './components/thematiques/thematiques.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import {UserGuard} from './services/user.guard';
     PublicationsComponent,
     ProfileComponent,
     FollowingComponent,
-    FollowedComponent
+    FollowedComponent,
+    ThematiquesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/thematiques/thematiques.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thematiques',
+  templateUrl: './thematiques.component.html',
+  styleUrls: ['./thematiques.component.css']
+})
+export class ThematiquesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
